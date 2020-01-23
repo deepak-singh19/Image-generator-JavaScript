dc70fb422a8089eb7221f54ea4dd26f86eaad04e
deepak-singh19 <48211820+deepak-singh19@users.noreply.github.com>
2020-01-24 00:56:47 +0530

    Add files via upload

---
+++ Project2.1/static/ji.js
@@ -0,0 +1,8 @@
+function f1()
+{
+    var image = document.createElement("img");
+    var m = document.getElementById("f")
+    image.src="https://media.giphy.com/media/a7dLN9ctIcyC4/giphy.gif"
+    image.style="width:210px;height:220px;"
+    m.appendChild(image)
+}
